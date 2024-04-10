63443e55cde8ce023d71bfc2110d4a0573583f52
Rasmus Sterup <69890797+Sterup15@users.noreply.github.com>
2024-04-10 14:02:49 +0200

    Implemented ViewModel abstract class

--- ClientModule/src/Views/ViewModel.java
+++ ClientModule/src/Views/ViewModel.java
@@ -1,4 +1,15 @@
 package Views;
 
+import Model.ClientModel;
+
 public abstract class ViewModel {
+    private ClientModel model;
+
+    public ViewModel(ClientModel model) {
+        this.model = model;
+    }
+
+    public ClientModel getModel() {
+        return model;
+    }
 }
