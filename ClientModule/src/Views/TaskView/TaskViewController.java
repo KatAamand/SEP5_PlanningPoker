9b703de45c4485315c75de78d05463ee79d3e05e
KatAamand <katrine.aamand@icloud.com>
2024-04-10 14:12:26 +0200

    Adding fields and button actions to .fxml and controller

--- ClientModule/src/Views/TaskView/TaskViewController.java
+++ ClientModule/src/Views/TaskView/TaskViewController.java
@@ -1,4 +1,24 @@
 package Views.TaskView;
 
+import Views.MainView.MainViewModel;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
 public class TaskViewController {
+    public VBox taskWrapper;
+    public VBox taskContainer;
+    public Label taskHeaderLabel;
+    public Label taskDescLabel;
+    private MainViewModel mainViewModel;
+    public TaskViewController(MainViewModel mainViewModel) {
+        this.mainViewModel = mainViewModel;
+    }
+
+    public void onCreateTaskButtonPressed(ActionEvent actionEvent) {
+    }
+
+    public void onEditTaskButtonPressed(ActionEvent actionEvent) {
+    }
 }
