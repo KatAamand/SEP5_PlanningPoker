67645f1f4f9c3ce137a0c829f6b89583e1e9c54a
KatAamand <katrine.aamand@icloud.com>
2024-04-10 13:40:03 +0200

    Login+Main: Adding controls to FXML and adding fields to Controllers

--- ClientModule/src/Views/MainView/MainViewController.java
+++ ClientModule/src/Views/MainView/MainViewController.java
@@ -1,9 +1,26 @@
 package Views.MainView;
 
 import Views.MainView.MainViewModel;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class MainViewController {
+    MainViewModel mainViewModel;
+
+    public Button createSessionButton;
+    public Button connectToSessionButton;
+    public TextField sessionIdTextField;
 
     public MainViewController(MainViewModel mainViewModel) {
+        this.mainViewModel = mainViewModel;
+    }
+
+    public void onCreateSessionButtonPressed(ActionEvent actionEvent) {
+        // Tries to create a session
+    }
+
+    public void onConnectToSessionButtonPressed(ActionEvent actionEvent) {
+        // Tries to connect to a session
     }
 }
