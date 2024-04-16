ca211cfe78eb68a7ce79be9f3192b730cafc7b1f
Rasmus Sterup <69890797+Sterup15@users.noreply.github.com>
2024-04-16 09:41:35 +0200

    Update ClientModel_Impl.java

--- ClientModule/src/Model/ClientModel_Impl.java
+++ ClientModule/src/Model/ClientModel_Impl.java
@@ -102,4 +102,9 @@ public class ClientModel_Impl implements ClientModel
   {
     propertyChangeSupport.removePropertyChangeListener(name, listener);
   }
+
+  @Override
+  public void sendMessage(String message) {
+    
+  }
 }
