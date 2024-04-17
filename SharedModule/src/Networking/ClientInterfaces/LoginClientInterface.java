ef568885a350a7fa272acf93f21221426cc1629e
KatAamand <katrine.aamand@icloud.com>
2024-04-17 11:58:10 +0200

    Updating with RemoteExceptions

--- SharedModule/src/Networking/ClientInterfaces/LoginClientInterface.java
+++ SharedModule/src/Networking/ClientInterfaces/LoginClientInterface.java
@@ -3,9 +3,19 @@ package Networking.ClientInterfaces;
 import DataTypes.User;
 import Util.PropertyChangeSubject;
 
-public interface LoginClientInterface extends PropertyChangeSubject {
+import java.beans.PropertyChangeListener;
+import java.rmi.RemoteException;
+
+public interface LoginClientInterface {
     void validateUser(String username, String password);
     void createUser(String username, String password);
     void userCreatedSuccessfully();
     void updateUser(User user);
+    void addPropertyChangeListener(PropertyChangeListener listener) throws RemoteException;
+
+    void addPropertyChangeListener(String name, PropertyChangeListener listener) throws RemoteException;
+
+    void removePropertyChangeListener(PropertyChangeListener listener) throws RemoteException;
+
+    void removePropertyChangeListener(String name, PropertyChangeListener listener) throws RemoteException;
 }
