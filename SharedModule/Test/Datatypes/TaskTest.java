ce92d8753b4fb32414b290b52e61beebec4395a8
Rasmus Sterup <69890797+Sterup15@users.noreply.github.com>
2024-05-01 08:32:25 +0200

    Made test-directories for all modules

---
+++ SharedModule/Test/Datatypes/TaskTest.java
@@ -0,0 +1,17 @@
+package Datatypes;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
