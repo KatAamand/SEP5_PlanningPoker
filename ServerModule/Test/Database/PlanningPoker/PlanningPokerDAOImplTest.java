3e28031d363743a65539de3a1456e70004c7be67
KatAamand <katrine.aamand@icloud.com>
2024-05-14 14:39:39 +0200

    UserDAO and PLanningPokerDAO UnitTests

--- ServerModule/Test/Database/PlanningPoker/PlanningPokerDAOImplTest.java
+++ ServerModule/Test/Database/PlanningPoker/PlanningPokerDAOImplTest.java
@@ -1,20 +1,87 @@
 package Database.PlanningPoker;
 
+import DataTypes.PlanningPoker;
+import DataTypes.User;
+import Database.User.UserDAOImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 class PlanningPokerDAOImplTest {
+    private PlanningPokerDAOImpl planningPokerDAO;
+    private Connection mockConnection;
+    private PreparedStatement mockPreparedStatement;
+    private ResultSet mockResultSet;
 
-    @Test
-    void create() {
+    @BeforeEach
+    void setUp() throws SQLException {
+        planningPokerDAO = PlanningPokerDAOImpl.getInstance();
+        mockConnection = Mockito.mock(Connection.class);
+        mockPreparedStatement = Mockito.mock(PreparedStatement.class);
+        mockResultSet = Mockito.mock(ResultSet.class);
+
+        when(mockConnection.prepareStatement(anyString())).thenReturn(mockPreparedStatement);
+        when(mockPreparedStatement.executeQuery()).thenReturn(mockResultSet);
     }
 
     @Test
-    void readByPlanningPoker() {
+    void createPlanningPoker() throws SQLException {
+        PlanningPoker planningpoker = planningPokerDAO.create();
+        assertNotNull(planningpoker);
     }
 
     @Test
-    void getAllPlanningPoker() {
+    void readByPlanningPoker() throws SQLException {
+        when(mockResultSet.next()).thenReturn(true);
+        when(mockResultSet.getString("planning_pokerid")).thenReturn("101");
+
+        PlanningPoker planningPoker = planningPokerDAO.readByPlanningPoker(101);
+        assertNotNull(planningPoker);
+        assertEquals("101", planningPoker.getPlanningPokerID());
+    }
+
+    @Test
+    void readByPlanningPoker_zero() throws SQLException {
+        when(mockResultSet.next()).thenReturn(false);
+
+        PlanningPoker planningPoker = planningPokerDAO.readByPlanningPoker(0);
+        assertNull(planningPoker);
+    }
+
+    @Test // Testing for the currently 225 rows in the planning_poker table
+    void getAllPlanningPoker_many() throws SQLException {
+        ArrayList<PlanningPoker> planningPokers = planningPokerDAO.getAllPlanningPoker();
+        assertEquals(225, planningPokers.size());
+    }
+
+    @Test // Something is not right with the PlanningPokerDAO class
+    void getAllPlanningPoker_objectsInTable() throws SQLException {
+        ArrayList<PlanningPoker> planningPokers = planningPokerDAO.getAllPlanningPoker();
+
+        assertEquals(101, planningPokers.get(0).getPlanningPokerID());
+        assertEquals(220, planningPokers.get(119).getPlanningPokerID());
+        assertEquals(224, planningPokers.get(325).getPlanningPokerID());
+    }
+
+    @Test
+    void readByPlanningPoker_IllegalArgument() throws SQLException {
+        when(mockConnection.prepareStatement(anyString())).thenThrow(new IllegalArgumentException());
+        PlanningPoker planningPoker = planningPokerDAO.readByPlanningPoker(0);
+    }
+
+    @Test
+    void testGetAllPlanningPoker_exception() throws SQLException {
+        when(mockConnection.prepareStatement(anyString())).thenThrow(new SQLException());
+        planningPokerDAO.getAllPlanningPoker();
     }
 }
--- ServerModule/src/Database/User/UserDAOImpl.java
+++ ServerModule/src/Database/User/UserDAOImpl.java
@@ -7,79 +7,75 @@ import Database.Connection.DatabaseConnection;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class UserDAOImpl extends DatabaseConnection implements UserDAO
-{
-  private static UserDAOImpl instance;
+public class UserDAOImpl extends DatabaseConnection implements UserDAO {
+    private static UserDAOImpl instance;
 
-  private UserDAOImpl() throws SQLException
-  {
-    DriverManager.registerDriver(new org.postgresql.Driver());
-  }
-
-  public static synchronized UserDAOImpl getInstance() throws SQLException
-  {
-    if (instance == null)
-    {
-      instance = new UserDAOImpl();
-    }
-    return instance;
-  }
-
-  @Override public User create(String username, String password)
-      throws SQLException
-  {
-    try (Connection connection = getConnection())
-
-    {
-      PreparedStatement statement = connection.prepareStatement(
-          "insert into \"user\"(username, password) VALUES (?,?);");
-      statement.setString(1, username);
-      statement.setString(2, password);
-      statement.executeUpdate();
-      return new User(username, password);
+    private UserDAOImpl() throws SQLException {
+        DriverManager.registerDriver(new org.postgresql.Driver());
     }
 
-  }
-
-  @Override public User readByLoginInfo(String username, String password)
-      throws SQLException
-  {
-    try (Connection connection = getConnection())
-    {
-      PreparedStatement statement = connection.prepareStatement(
-          "SELECT * FROM \"user\" WHERE username = ? AND password = ?");
-      statement.setString(1, username);
-      statement.setString(2, password);
-      ResultSet resultSet = statement.executeQuery();
-
-      if (resultSet.next()) {
-        String retrievedUsername = resultSet.getString("username");
-        String retrievedPassword = resultSet.getString("password");
-        return new User(retrievedUsername, retrievedPassword);
-      } else {
-        return null;
-      }
+    public static synchronized UserDAOImpl getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new UserDAOImpl();
+        }
+        return instance;
     }
-  }
 
-  @Override
-  public ArrayList<User> getAllUsers() throws SQLException {
-    try (Connection connection = getConnection()) {
-      PreparedStatement statement = connection.prepareStatement("SELECT * FROM \"user\"");
-      ResultSet resultSet = statement.executeQuery();
-      ArrayList<User> users = new ArrayList<>();
-      while (resultSet.next()) {
-        String username = resultSet.getString("username");
-        String password = resultSet.getString("password");
-        users.add(new User(username, password));
-      }
-      return users;
+    @Override
+    public User create(String username, String password)
+            throws SQLException {
+        if (username == null || password == null) {
+            throw new IllegalArgumentException("Username and password cannot be null");
+        }
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(
+                    "insert into \"user\"(username, password) VALUES (?,?);");
+            statement.setString(1, username);
+            statement.setString(2, password);
+            statement.executeUpdate();
+            return new User(username, password);
+        }
+
     }
-  }
 
-  @Override public Connection getConnection() throws SQLException
-  {
+    @Override
+    public User readByLoginInfo(String username, String password)
+            throws SQLException {
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT * FROM \"user\" WHERE username = ? AND password = ?");
+            statement.setString(1, username);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
 
-    return super.getConnection();
-  }
+            if (resultSet.next()) {
+                String retrievedUsername = resultSet.getString("username");
+                String retrievedPassword = resultSet.getString("password");
+                return new User(retrievedUsername, retrievedPassword);
+            } else {
+                return null;
+            }
+        }
+    }
+
+    @Override
+    public ArrayList<User> getAllUsers() throws SQLException {
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM \"user\"");
+            ResultSet resultSet = statement.executeQuery();
+            ArrayList<User> users = new ArrayList<>();
+            while (resultSet.next()) {
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+                users.add(new User(username, password));
+            }
+            return users;
+        }
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+
+        return super.getConnection();
+    }
 }
