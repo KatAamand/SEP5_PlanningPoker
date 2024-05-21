98e662dd55b9a2a14ca6eb8b0bad6a8f98e8daa0
Rasmus Sterup <69890797+Sterup15@users.noreply.github.com>
2024-05-21 10:47:09 +0200

    Voice chat test

--- ClientModule/src/Networking/VoiceChat/VoiceChatClient.java
+++ ClientModule/src/Networking/VoiceChat/VoiceChatClient.java
@@ -1,4 +1,4 @@
-package Networking.VoiceChat;
+/* package Networking.VoiceChat;
 
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
@@ -112,5 +112,132 @@ public class VoiceChatClient {
         microphone.close();
         speakers.stop();
         speakers.close();
+    }
+}
+
+ */
+
+package Networking.VoiceChat;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.TargetDataLine;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class VoiceChatClient {
+    private static final String SERVER_ADDRESS = "192.168.1.166";  // Skift til serverens IP-adresse
+    private static int serverPort;
+    private DatagramSocket clientSocket;
+    private Thread sendThread;
+    private Thread receiveThread;
+    private volatile boolean running;
+    private TargetDataLine microphone;
+    private SourceDataLine speakers;
+    private static VoiceChatClient instance;
+    private static final Lock lock = new ReentrantLock();
+
+    private VoiceChatClient(int serverPort) {
+        try {
+            microphone = AudioUtils.getTargetDataLine();
+            speakers = AudioUtils.getSourceDataLine();
+            clientSocket = new DatagramSocket();
+        } catch (LineUnavailableException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        this.serverPort = serverPort;
+    }
+
+    public static VoiceChatClient getInstance(int serverPortToSet) {
+        lock.lock();
+        try {
+            if (instance == null) {
+                instance = new VoiceChatClient(serverPortToSet);
+            }
+            serverPort = serverPortToSet;
+        } finally {
+            lock.unlock();
+        }
+        return instance;
+    }
+
+    public void start() {
+        if (running) return;
+
+        running = true;
+        try {
+            microphone.open();
+            microphone.start();
+            speakers.open();
+            speakers.start();
+        } catch (LineUnavailableException e) {
+            throw new RuntimeException(e);
+        }
+
+        sendThread = new Thread(() -> {
+            try {
+                InetAddress serverAddress = InetAddress.getByName(SERVER_ADDRESS);
+                byte[] buffer = new byte[1024];
+
+                while (running) {
+                    int bytesRead = microphone.read(buffer, 0, buffer.length);
+                    DatagramPacket sendPacket = new DatagramPacket(buffer, bytesRead, serverAddress, serverPort);
+                    clientSocket.send(sendPacket);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        receiveThread = new Thread(() -> {
+            try {
+                byte[] receiveBuffer = new byte[1024];
+
+                while (running) {
+                    DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                    clientSocket.receive(receivePacket);
+                    speakers.write(receivePacket.getData(), 0, receivePacket.getLength());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        sendThread.start();
+        receiveThread.start();
+    }
+
+    public void stop() {
+        running = false;
+        try {
+            if (sendThread != null) {
+                sendThread.join();
+            }
+            if (receiveThread != null) {
+                receiveThread.join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            if (microphone != null) {
+                microphone.stop();
+                microphone.close();
+            }
+            if (speakers != null) {
+                speakers.stop();
+                speakers.close();
+            }
+            if (clientSocket != null && !clientSocket.isClosed()) {
+                clientSocket.close();
+            }
+        }
     }
 }
