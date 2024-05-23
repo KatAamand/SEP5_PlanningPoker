1d696473bb366fb02cf19455b0d7e4b2938d2c7e
KatAamand <katrine.aamand@icloud.com>
2024-05-23 09:55:13 +0200

    Adding source code comments to MainServerModel

--- ServerModule/src/Model/Main/MainServerModel.java
+++ ServerModule/src/Model/Main/MainServerModel.java
@@ -14,8 +14,20 @@ import java.util.Map;
 
 public interface MainServerModel extends PropertyChangeSubject
 {
+
+  /**
+   * Validates the existence of a Planning Poker game with the specified ID.
+   * @param planningPokerID the ID of the Planning Poker game to validate.
+   * @return true if the game exists, false otherwise.
+   */
   boolean validatePlanningPoker(int planningPokerID);
   PlanningPoker createPlanningPoker();
+
+  /**
+   * Retrieves the Planning Poker game with the specified ID.
+   * @param planningPokerId the ID of the Planning Poker game to retrieve.
+   * @return the PlanningPoker object if found, null otherwise.
+   */
   PlanningPoker getPlanningPokerGame(int planningPokerId);
   void getAllPlanningPokersFromDb();
   User applyPlanningPokerRole(UserRole role, User user, int planningPokerId);
