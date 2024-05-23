79e298edb8ce656908b88123201f9ffab442aa2c
Rasmus Sterup <69890797+Sterup15@users.noreply.github.com>
2024-05-23 11:43:56 +0200

    Added code comments

--- ServerModule/src/Model/Main/MainServerModel.java
+++ ServerModule/src/Model/Main/MainServerModel.java
@@ -14,12 +14,31 @@ import java.util.Map;
 
 public interface MainServerModel extends PropertyChangeSubject
 {
+
+  /**
+   * Validates the existence of a Planning Poker game with the specified ID.
+   * @param planningPokerID the ID of the Planning Poker game to validate.
+   * @return true if the game exists, false otherwise.
+   */
   boolean validatePlanningPoker(int planningPokerID);
   PlanningPoker createPlanningPoker();
+
+  /**
+   * Retrieves the Planning Poker game with the specified ID.
+   * @param planningPokerId the ID of the Planning Poker game to retrieve.
+   * @return the PlanningPoker object if found, null otherwise.
+   */
   PlanningPoker getPlanningPokerGame(int planningPokerId);
+
+  /** Retrieves all saved planning poker session from the database and saves them in the model*/
   void getAllPlanningPokersFromDb();
   User applyPlanningPokerRole(UserRole role, User user, int planningPokerId);
   void broadcastPlanningPokerObjUpdate(Map<Integer, ArrayList<ClientConnection_RMI>> clientList, ServerConnection_RMI server, int planningPokerId);
+
+  /** Sets the product owner for the users planning poker session
+   * @param user the user that is intended to become product owner
+   * @param connectedClients all the clients that are connected to the server
+   * @param serverRmi the server which has received the request*/
   void setProductOwner(User user, ArrayList<ClientConnection_RMI> connectedClients, Server_RMI serverRmi);
   boolean removeUserFromGame(User user, int planningPokerId);
   void verifyConnectedUsersIntegrity(Map<Integer, ArrayList<ClientConnection_RMI>> clientList, ServerConnection_RMI server, int planningPokerId);
