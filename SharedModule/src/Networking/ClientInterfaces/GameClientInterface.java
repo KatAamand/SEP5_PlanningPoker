ab7b9b859010a0791a272a16b53b72cd15cc276a
KatAamand <katrine.aamand@icloud.com>
2024-05-23 10:02:05 +0200

    Adding source code comments for gameClient, LobbyClient and LoginClient

--- SharedModule/src/Networking/ClientInterfaces/GameClientInterface.java
+++ SharedModule/src/Networking/ClientInterfaces/GameClientInterface.java
@@ -7,12 +7,38 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface GameClientInterface extends Remote {
+
+    /**
+     * Receives the placed card data from the server.
+     * @param userCardData the data of the card placed by the user.
+     * @throws RemoteException if there is a communication error during the remote method call.
+     */
     void receivePlacedCard(UserCardData userCardData) throws RemoteException;
+
+    /**
+     * Clears all placed cards.
+     * @throws RemoteException if there is a communication error during the remote method call.
+     */
     void clearPlacedCards() throws RemoteException;
 
+
+    /**
+     * Displays all placed cards.
+     * @throws RemoteException if there is a communication error during the remote method call.
+     */
     void showCards() throws RemoteException;
 
+
+    /**
+     * Receives the recommended effort from the server.
+     * @param recommendedEffort the recommended effort as a string.
+     * @throws RemoteException if there is a communication error during the remote method call.
+     */
     void receiveRecommendedEffort(String recommendedEffort) throws RemoteException;
 
+    /**
+     * Starts the timer for the game.
+     * @throws RemoteException if there is a communication error during the remote method call.
+     */
     void startTimer() throws RemoteException;
 }
--- SharedModule/src/Networking/ClientInterfaces/LoginClientInterface.java
+++ SharedModule/src/Networking/ClientInterfaces/LoginClientInterface.java
@@ -6,11 +6,43 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface LoginClientInterface extends Remote {
+    /**
+     * Validates a user's credentials.
+     * @param username the username of the user to validate.
+     * @param password the password of the user to validate.
+     * @throws RemoteException if there is a communication error during the remote method call.
+     */
     void validateUser(String username, String password) throws RemoteException;
+
+    /**
+     * Creates a new user with the specified username and password.
+     * @param username the desired username for the new user.
+     * @param password the desired password for the new user.
+     * @throws RemoteException if there is a communication error during the remote method call.
+     */
     void createUser(String username, String password) throws RemoteException;
+
+    /**
+     * Notifies the client that user validation has failed.
+     * @param errorMessage the error message describing the validation failure.
+     * @throws RemoteException if there is a communication error during the remote method call.
+     */
     void userValidationFailed(String errorMessage) throws RemoteException;
+
+
+    /**
+     * Notifies the client that a user has been successfully created.
+     * @throws RemoteException if there is a communication error during the remote method call.
+     */
     void userCreatedSuccessfully() throws RemoteException;
+
+    /**
+     * Updates the client's information with the specified user data.
+     * @param user the user data to update.
+     * @throws RemoteException if there is a communication error during the remote method call.
+     */
     void updateUser(User user) throws RemoteException;
+
     void addPropertyChangeListener(PropertyChangeListener listener) throws RemoteException;
 
     void addPropertyChangeListener(String name, PropertyChangeListener listener) throws RemoteException;
@@ -18,5 +50,6 @@ public interface LoginClientInterface extends Remote {
     void removePropertyChangeListener(PropertyChangeListener listener) throws RemoteException;
 
     void removePropertyChangeListener(String name, PropertyChangeListener listener) throws RemoteException;
+
     void logoutUserFromServer(String username, String password) throws RemoteException;
 }
