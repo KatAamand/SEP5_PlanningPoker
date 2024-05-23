62527b34b4bf770055e0b0e01487be0006a35777
Kristian <344849@via.dk>
2024-05-23 16:03:01 +0200

    Added source code comments to the interface ChatClientInterface.java.

--- ClientModule/src/Networking/Client_RMI_Impl.java
+++ ClientModule/src/Networking/Client_RMI_Impl.java
@@ -364,7 +364,6 @@ public class Client_RMI_Impl implements Client, ClientConnection_RMI, Serializab
     @Override
     public void receiveUser(ArrayList<User> users) throws RemoteException {
         Platform.runLater(() -> {
-            //System.out.println("Client receiving user" + users);
             propertyChangeSupport.firePropertyChange("userReceived", null, users);
             propertyChangeSupport.firePropertyChange("UpdatedLocalUser", null, null);
         });
--- SharedModule/src/Networking/ClientInterfaces/ChatClientInterface.java
+++ SharedModule/src/Networking/ClientInterfaces/ChatClientInterface.java
@@ -2,15 +2,31 @@ package Networking.ClientInterfaces;
 
 import DataTypes.Message;
 import DataTypes.User;
-
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
-public interface ChatClientInterface extends Remote {
-    void sendMessage(Message message, User sender) throws RemoteException;
-    void receiveMessage(Message message)  throws RemoteException;
-    User getCurrentUser() throws RemoteException;
-    void receiveUser(ArrayList<User> users) throws RemoteException;
 
+/**
+ * The ChatClientInterface interface defines the key methods relating to interfacing the client-side application with the server,
+ * as they relate to the chat related network interactions.
+ * These methods enable the server and client to communicate with each other, and can be called from either the server or the client.
+ * It extends the Remote interface and is thus prepared for network use with RMI.
+ */
+public interface ChatClientInterface extends Remote {
+
+    // TODO: Source code comment/document
+    void sendMessage(Message message, User sender) throws RemoteException; //TODO: Method is never used. Should be removed from this interface.
+
+
+    // TODO: Source code comment/document
+    void receiveMessage(Message message)  throws RemoteException;
+
+
+    // TODO: Source code comment/document
+    User getCurrentUser() throws RemoteException;
+
+
+    // TODO: Source code comment/document
+    void receiveUser(ArrayList<User> users) throws RemoteException;
 }
